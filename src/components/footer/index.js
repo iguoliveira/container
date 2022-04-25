4f82ff03ea2791426d69e1d4eb7effb5cd1798a9
Igor Oliveira <57506257+iguoliveira@users.noreply.github.com>
2022-04-25 16:58:12 -0300

    Updated the Footer
    ##MADE
    Added the sections of the footer
    
    ##TO-DO
    Align the first section
    Customize the final section

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,8 +3,23 @@ import './index.css'
 export default function Footer(){
     return(
         <footer className='footer'>
-            <div className='text'>
-                <h5>Designed and made by Igor Oliveira Rodrigues</h5>
+            <div className='about-me'>
+                <div className='about-me-header'>IGOR OLIVEIRA RODRIGUES</div>
+                <div className='about-me-text'>Sou um desenvolvedor web!</div>
+            </div>
+
+            <div className='personal-projects'>
+                <div className='personal-projects-header'>SOME PROJECTS</div>
+                <div className='personal-projects-list'>
+                    <div><a href='https://github.com/iguoliveira/calculatorWeb' target={ '_blank' }>Web Calculator</a></div>
+                    <div><a href='https://github.com/iguoliveira/javaCalculator' target={ '_blank' }>Java Calculator</a></div>
+                </div>
+            </div>
+
+            <div className='midia-links'>
+                <div>Github</div>
+                <div>Linkedos</div>
+                <div>Twitter</div>
             </div>
         </footer>
     )
