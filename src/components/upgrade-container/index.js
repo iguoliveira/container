0c83f749eff0a52f9e379badeb9f32919e3b33ad
Igor Oliveira <57506257+iguoliveira@users.noreply.github.com>
2022-05-03 11:00:27 -0300

    Add files via upload

--- src/components/upgrade-container/index.js
+++ src/components/upgrade-container/index.js
@@ -1,14 +1,28 @@
+import React, { useEffect, useState } from "react";
 import "./index.css"
 
-export default function UpgradeContainer({preco, setPreco, clicks,setClicks}){
+export function UpgradeContainer({preco, setPreco, clicks,setClicks}){
+    const [ isQtd, setIsQtd] = useState(0)
+
+    useEffect(() => {
+        if(isQtd >= 1){
+            setTimeout(() => {
+                setClicks(clicks+0.01)
+            }, 1000)
+        }
+    }, [ clicks ])
+
+
     return(
         <div>
-            <div>UPGRADE 1</div>
+            <div>MORIKO</div>
             <div>PRECO: {preco}</div>
+            <div>QTD: { isQtd }</div>
             <button onClick={() => {
                 if(clicks >= preco){
                     setClicks(clicks - preco)
                     setPreco(Math.floor((preco/2)*3))
+                    setIsQtd(isQtd+1)
                 }else{
                     console.log("SEM MONEY")
                 }
