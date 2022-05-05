283fda90708632d9de96259bbf474645d7f65c6e
iguoliveira <igor.or042@gmail.com>
2022-05-05 11:15:33 -0300

    Implemented the begining of Login with google function and hosted
    Login with google account in progress, with interaction between the components and hosted the site. Everything with firebase. NEED TO FIX THE LOGOUT BUTTON

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,12 @@
+import { Login } from "./login/Login"
+
 export function Navbar(){
     return(
         <div className="flex justify-between p-3 bg-navbar-1000">
             <a href="index.html"><div className="text-xl font-bold hover:text-slate-600 transition-all duration-250 ease-linear">WAIFU CLICKER</div></a>
-            <div className="text-lg hover:text-slate-600 transition-all duration-250 ease-linear">
-                <a href="https://github.com/iguoliveira/web-clicker" target={'_blank'}><div>GITHUB</div></a>
+            <div className="text-lg flex gap-5">
+                <Login />
+                <a href="https://github.com/iguoliveira/web-clicker" target={'_blank'}><div className="hover:text-slate-600 transition-all duration-250 ease-linear">Github</div></a>
             </div>
         </div>
     )
---
+++ src/components/navbar/login/Login.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react"
+import useAuth from "../../../hooks/useAuth"
+import { firebase, auth } from "./../../../service/firebase"
+import { Logout } from "./Logout"
+
+export function Login(){
+    const { user, setUser } = useAuth()
+    const [isLogin, setIsLogin] = useState(false)
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if(user){
+                const { uid, displayName, photoURL } = user
+                if(!displayName || !photoURL) throw new Error("User doesn't have DisplayName or URL Photo")
+    
+                setUser({
+                    id: uid,
+                    avatar: photoURL,
+                    name: displayName
+                })
+            }
+        })
+    },[])
+
+    const handleClickLogout = async () => {
+        firebase.auth().signOut().then(function() {
+            console.log("SIGNED OUT")
+            console.log(user.name)
+        })
+    }
+
+    const handleClickLogin = async () => {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        const result = await auth.signInWithPopup(provider)
+        const xxx = firebase.auth().signOut
+
+        if(result.user){
+            const { uid, displayName, photoURL } = result.user
+            setIsLogin(true)
+            if(!displayName || !photoURL) throw new Error("User doesn't have DisplayName or URL Photo")
+
+            setUser({
+                id: uid,
+                avatar: photoURL,
+                name: displayName
+            })
+        }
+    }
+
+    return (
+        <div>
+            {isLogin ? <Logout name={user.name} clickHandler={handleClickLogout}/> : <button onClick={handleClickLogin} className="hover:text-slate-600 transition-all duration-250 ease-linear">Login</button>}
+        </div>
+    )
+}
