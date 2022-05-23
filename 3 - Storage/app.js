8f7137addf2551d43889552f71f4890fa0377ccd
iguoliveira <igor.or042@gmail.com>
2022-05-23 11:21:52 -0300

    First commit

---
+++ 3 - Storage/app.js
@@ -0,0 +1,19 @@
+/**
+ * Variáveis com referencias dos inputs
+ */
+var fileInput = document.getElementById('file-input');
+var stringInput = document.getElementById('string-input');
+
+/**
+ * Metodo que observa mudanças no input de arquivo
+ */
+fileInput.onchange = function (event) {
+    
+}
+
+/**
+ * Metodo que observa mudanças no input de string
+ */
+stringInput.onchange = function (event) {
+
+}
---
+++ 4 - Authentication/js/email.js
@@ -0,0 +1,20 @@
+/**
+ * Função para cadastro com email e senha
+ */
+function createLogin() {
+
+}
+
+/**
+ * Função para login
+ */
+function loginEmail() {
+
+}
+
+/**
+ * Listener de dom ready
+ */
+document.addEventListener("DOMContentLoaded", function () {
+
+});
