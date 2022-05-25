ee867332b4116c8a6d1e7327420311d2421d8f8c
iguoliveira <igor.or042@gmail.com>
2022-05-25 11:20:38 -0300

    fixed the remove settings. ##TODO move my msgs to right side

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
+// Firebase settings
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getDatabase, ref, set, push, remove, onValue } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
+import { getDatabase, ref, push, remove, onValue } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
 
 var firebaseConfig = {
     apiKey: "AIzaSyDUpO6ZYLonxNkz1Oll-XKA0QyVrD2jRn8",
@@ -11,18 +12,18 @@ var firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 var db = getDatabase(app);
 const dbRef = ref(db, 'users');
 
+// Check the fields and send the message
 document.getElementById("btnSend").addEventListener("click", (() => {
     if(document.getElementById("name").value != ""){
         document.getElementById("name").setAttribute("readonly", "")
         if(document.getElementById("msg").value != ""){
-            if(document.getElementById("msg").value.length >= 5 && document.getElementById("msg").value.length <=100){
+            if(document.getElementById("msg").value.length >= 2 && document.getElementById("msg").value.length <= 100){
                 enviarMSG()
             }else{
-                alert("Message with a wrong size! Reminder: min 5chars || max 100chars")
+                alert("Message with a wrong size! Reminder: min 2chars || max 100chars")
             }
         }else{
             alert("Send something, doesn't let in blank!")
@@ -34,58 +35,57 @@ document.getElementById("btnSend").addEventListener("click", (() => {
 
 function deleteMessage(key){
     remove(ref(db, 'users/' + key))
-    console.log(document.getElementById(key).parentElement)
-    document.getElementById(key).parentElement.remove()
+    document.getElementById(key).remove()
 }
 
+// Create the new component to the message
 onValue(dbRef, (snapshot) => {
-    const data = snapshot.val();
-    console.log(data);
-    let div = ""
-
+    document.getElementById("content").innerHTML = ""
     snapshot.forEach(function (childSnapshot) {
-        var key = childSnapshot.key;
-        
-        console.log(key);
-        console.log(childSnapshot.val().nome);
-        console.log(childSnapshot.val().mensagem);
+        let key = childSnapshot.key
 
+        let btnDelete = document.createElement('div')
+        btnDelete.classList.add("btnDelete")
+        btnDelete.setAttribute("id", key+childSnapshot.val().nome)
+        btnDelete.textContent = "Delete"
         let name = document.createElement('div')
         name.classList.add('divName')
-        name.textContent = `${childSnapshot.val().nome}:`
+        name.textContent = childSnapshot.val().nome
+        name.appendChild(btnDelete)
 
         let msg = document.createElement('div')
         msg.classList.add('divMsg')
-        msg.setAttribute('id', key)
-        msg.textContent = `${childSnapshot.val().mensagem}`
+        msg.textContent = childSnapshot.val().mensagem
 
         let time = document.createElement('div')
         time.classList.add('divTime')
         time.textContent = childSnapshot.val().horario
 
-        let btnDelete = document.createElement('div')
-        btnDelete.classList.add("btnDelete")
-        btnDelete.setAttribute("id", key)
-        btnDelete.textContent = "Delete"
 
-        div = document.createElement('div')
+
+        let div = document.createElement('div')
         div.classList.add('msg')
+        div.setAttribute("id", key)
         div.appendChild(name)
         div.appendChild(msg)
         div.appendChild(time)
+        
 
-        if(childSnapshot.val().nome == document.getElementById("name").value){
-            btnDelete.onclick = () => {
-                deleteMessage(key)
-            }
-            div.appendChild(btnDelete)
+        if(childSnapshot.val().nome != document.getElementById("name").value){
+            // document.getElementById('content').style.alignItems = 'start;'
+            btnDelete.style = 'display:none;'
+        }else{
+            changeMsgStyle(div)
+        }
+        
+        atualizarHTML(div);
+        document.getElementById(key + childSnapshot.val().nome).onclick = () => {
+            deleteMessage(key)
         }
     });
-    atualizarHTML(div);
 });
 
 function enviarMSG() {
-
     var datahj = new Date();
     var hora = datahj.getHours() + ":" + datahj.getMinutes() + ":" + datahj.getSeconds()
 
@@ -98,11 +98,17 @@ function enviarMSG() {
 
 function atualizarHTML(value) {
     document.getElementById("content").appendChild(value)
-    ajustarScroll();
+    // ajustarScroll();
 }
 
-function ajustarScroll() {
-    console.log("corrirgir scroll");
-    var divConteudo = document.getElementById("content");
-    divConteudo.scrollTop = divConteudo.scrollHeight;
+function changeMsgStyle(value){
+    // document.getElementById('content').style.alignItems = 'end'
+    value.classList.remove('msg')
+    value.classList.add('myMsg')
 }
+
+// function ajustarScroll() {
+//     console.log("corrirgir scroll");
+//     var divConteudo = document.getElementById("content");
+//     divConteudo.scrollTop = divConteudo.scrollHeight;
+// }
