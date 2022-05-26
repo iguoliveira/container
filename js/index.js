c38ca90aadf5a09579bc0329fc596e2c91ce3602
iguoliveira <igor.or042@gmail.com>
2022-05-26 09:52:42 -0300

    Edited the style and make the div scrollable

--- js/index.js
+++ js/index.js
@@ -22,6 +22,7 @@ document.getElementById("btnSend").addEventListener("click", (() => {
         if(document.getElementById("msg").value != ""){
             if(document.getElementById("msg").value.length >= 2 && document.getElementById("msg").value.length <= 100){
                 enviarMSG()
+                document.getElementById("msg").value = ""
             }else{
                 alert("Message with a wrong size! Reminder: min 2chars || max 100chars")
             }
@@ -33,14 +34,15 @@ document.getElementById("btnSend").addEventListener("click", (() => {
     }
 }))
 
+// Delete message from DB
 function deleteMessage(key){
     remove(ref(db, 'users/' + key))
-    document.getElementById(key).remove()
 }
 
-// Create the new component to the message
+// Create the new component for the message
 onValue(dbRef, (snapshot) => {
-    document.getElementById("content").innerHTML = ""
+    document.getElementById('content').innerHTML = ''
+
     snapshot.forEach(function (childSnapshot) {
         let key = childSnapshot.key
 
@@ -48,6 +50,7 @@ onValue(dbRef, (snapshot) => {
         btnDelete.classList.add("btnDelete")
         btnDelete.setAttribute("id", key+childSnapshot.val().nome)
         btnDelete.textContent = "Delete"
+
         let name = document.createElement('div')
         name.classList.add('divName')
         name.textContent = childSnapshot.val().nome
@@ -55,13 +58,12 @@ onValue(dbRef, (snapshot) => {
 
         let msg = document.createElement('div')
         msg.classList.add('divMsg')
+        msg.setAttribute("id", key+key)
         msg.textContent = childSnapshot.val().mensagem
 
         let time = document.createElement('div')
         time.classList.add('divTime')
         time.textContent = childSnapshot.val().horario
-
-
 
         let div = document.createElement('div')
         div.classList.add('msg')
@@ -69,22 +71,31 @@ onValue(dbRef, (snapshot) => {
         div.appendChild(name)
         div.appendChild(msg)
         div.appendChild(time)
+
+        let newDiv = document.createElement('div')
+        newDiv.classList.add('userMsg-container')
+        newDiv.appendChild(div)
         
 
         if(childSnapshot.val().nome != document.getElementById("name").value){
-            // document.getElementById('content').style.alignItems = 'start;'
             btnDelete.style = 'display:none;'
+            atualizarHTML(div);
         }else{
-            changeMsgStyle(div)
+            div.style.backgroundColor = '#F8F2F5'
+            atualizarHTML(newDiv);
         }
         
-        atualizarHTML(div);
         document.getElementById(key + childSnapshot.val().nome).onclick = () => {
+            btnDelete.style = 'display:none;'
             deleteMessage(key)
+            atualizarHTML(newDiv)
+            console.log(newDiv)
+            document.getElementById(key+key).innerHTML = "This message has been deleted!"
         }
     });
 });
 
+// Send the data to the DB
 function enviarMSG() {
     var datahj = new Date();
     var hora = datahj.getHours() + ":" + datahj.getMinutes() + ":" + datahj.getSeconds()
@@ -96,19 +107,15 @@ function enviarMSG() {
     });
 }
 
+// Update the DOM everytime the page was updated
 function atualizarHTML(value) {
     document.getElementById("content").appendChild(value)
     // ajustarScroll();
 }
 
-function changeMsgStyle(value){
-    // document.getElementById('content').style.alignItems = 'end'
-    value.classList.remove('msg')
-    value.classList.add('myMsg')
-}
-
 // function ajustarScroll() {
-//     console.log("corrirgir scroll");
 //     var divConteudo = document.getElementById("content");
 //     divConteudo.scrollTop = divConteudo.scrollHeight;
+// 
+//     PEGA A DIV CONTEUDO E ROLA PARA O TAMANHO MAXIMO DELA
 // }
