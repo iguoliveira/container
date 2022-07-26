0cc8ced53616d7101694dd04095e8abe81106364
iguoliveira <igor.or042@gmail.com>
2022-07-26 11:03:21 -0300

    Showing database data in HTML

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from polls.models import Person
+
 
 def home(request):
-    return render(request, 'homepage.html')
+    sql = Person.objects.all()
+
+    context = {
+        'person_info':sql,
+    }
+
+    return render(request, 'homepage.html', context)
