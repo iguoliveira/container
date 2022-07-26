795f9eb71a11433fa1051e2e49b7913fefe520fd
iguoliveira <igor.or042@gmail.com>
2022-07-26 11:37:19 -0300

    Return object list and create the list page

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home)
+    path('', views.home, name='home'),
+    path('list/', views.list, name='list')
+    # path('list/<int:id>', views.list, name='list') <- URL with type and valuable
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 from polls.models import Person
 
 
 def home(request):
+    return render(request, 'homepage.html')
+
+def list(request):
+    # # personInfo = get_object_or_404(
+    # #     Person, id=id
+    # # )
+    # return render(request, 'list.html', {
+    #     'details':personInfo
+    # })
+    
     sql = Person.objects.all()
 
     context = {
         'person_info':sql,
     }
 
-    return render(request, 'homepage.html', context)
+    return render(request, 'list.html', context)
