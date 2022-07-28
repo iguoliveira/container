4147d1ad3913bd39580359791c395ae17aa983c9
iguoliveira <igor.or042@gmail.com>
2022-07-28 09:46:58 -0300

    List and detail page

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('detalhes/<int:id>', views.detalhes, name='detalhes'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from home.models import Curso
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    curso_dados = Curso.objects.all()
+    context = {
+        'curso': curso_dados,
+    }
+    return render(request, 'home/index.html', context)
+
+
+def detalhes(request, id):
+    det_dados = get_object_or_404(
+        Curso, id=id
+    )
+    return render(request, 'home/detalhes.html', {'detalhe': det_dados})
