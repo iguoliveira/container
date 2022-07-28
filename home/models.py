cd0e3256ba4d1b5bab25d41d97918d74ee4448d4
iguoliveira <igor.or042@gmail.com>
2022-07-28 11:34:51 -0300

    Adding photos field to db

--- home/models.py
+++ home/models.py
@@ -1,6 +1,6 @@
-from string import digits
+from distutils.command.upload import upload
 from django.db import models
-from django.forms import CharField
+from stdimage import StdImageField
 
 # Create your models here.
 
@@ -10,6 +10,7 @@ class Curso(models.Model):
     descricao = models.CharField(max_length=200)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     ativo = models.BooleanField()
+    foto = StdImageField(upload_to='foto/%y/%m/%d/', variations={'pequena': (300, 300), 'media': (600, 600), 'grande': (800, 800),})
 
     def __str__(self):
         return self.nome
