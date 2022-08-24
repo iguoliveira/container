cb4cb2ff731c6de2f2540e37054974356a25874f
iguoliveira <igor.or042@gmail.com>
2022-08-24 11:35:14 -0300

    Started Navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,7 +1,16 @@
 export const Navbar = () => {
-    return(
+  return (
+    <div className="w-screen bg-pink-500 py-10">
+      <div className="flex justify-between px-10">
         <div>
-            <div>sim</div>
+          <div>logo</div>
         </div>
-    )
-}
+        <div className="flex">
+          <div>LINKS 1</div>
+          <div>LINKS 2</div>
+          <div>LINKS 3</div>
+        </div>
+      </div>
+    </div>
+  );
+};
