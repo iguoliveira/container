7195384e7037185f28ec0eb66818874257ab3e77
iguoliveira <igor.or042@gmail.com>
2022-08-24 11:13:38 -0300

    Fixed tailwindcss

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+    return(
+        <div>
+            <div>sim</div>
+        </div>
+    )
+}
---
+++ frontend/src/routes/Homepage.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from "../components/navbar/Navbar"
+
+export const Homepage = () => {
+    return(
+        <div>
+            <Navbar />
+        </div>
+    )
+}
