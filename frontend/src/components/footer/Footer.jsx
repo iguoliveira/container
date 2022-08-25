0158ed55099ef2305926a9d8e531437e834bf681
igor.or042@gmail.com <igor.or042@gmail.com>
2022-08-25 11:32:11 -0300

    Created char card + Consume api in char list page + Responsive card

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <div className="h-screen bg-gray-900 flex justify-between py-4 px-4">
+        <div className="bg-gray-900 flex justify-between py-4 px-4">
             <div className="text-white">
                 <div className="w-40">
                     Made By Igor Oliveira Rodrigues @2022
