f73ce2edd42f3744d999f909f59c0d3e6a50f007
igor.or042@gmail.com <04409605259>
2022-08-26 08:07:15 -0300

    Made image responsive + Border for rank

--- src/components/card/CharCard.jsx
+++ src/components/card/CharCard.jsx
@@ -1,8 +1,8 @@
 export const CharCard = (props) => {
     return(
         <div className="bg-black text-white w-full h-full rounded-xl p-5">
-            <div>
-                <img src={props.photo} />
+            <div className="flex justify-center bg-[]">
+                <img className="float-left w-40 h-40 object-cover rounded border-8 border-[#FFD700]" src={props.photo} />
             </div>
             <div>
                 <div className="text-center uppercase">{props.name}</div>
