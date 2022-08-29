cd3e59456bbd66f9dfb4dbc85f6c38522f101e6a
iguoliveira <igor.or042@gmail.com>
2022-08-29 11:36:45 -0300

    Changes

--- heroes/api/viewsets.py
+++ heroes/api/viewsets.py
@@ -2,9 +2,11 @@ from rest_framework import viewsets
 from heroes.api import serializers
 from heroes import models
 
+
 class RanksViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.RanksSerializer
     queryset = models.Rank.objects.all()
+
 
 class HeroesViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.HeroesSerializer
