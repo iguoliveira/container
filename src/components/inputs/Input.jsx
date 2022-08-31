0dafe9d59bdd60c27a9d6f48a4c33afa9a61faa9
iguoliveira <igor.or042@gmail.com>
2022-08-31 11:22:05 -0300

    Added state value for input

--- src/components/inputs/Input.jsx
+++ src/components/inputs/Input.jsx
@@ -1,3 +1,3 @@
-export const Input = () => {
-    return <input className="border-2 border-black rounded-lg"/>
+export const Input = (props) => {
+    return <input className="border-b-2 border-black rounded-lg p-1" type={props.type} placeholder={props.placeholder} value={props.value} onChange={props.onchange}/>
 }
--- src/routes/Editpage.jsx
+++ src/routes/Editpage.jsx
@@ -1,18 +1,22 @@
 import { Navbar } from "../components/navbar/Navbar";
 import { Input } from "../components/inputs/Input";
+import { useState } from "react";
 
 export const Editpage = () => {
+    const [heroname, setHeroname] = useState("")
+
     return(
         <div>
             <Navbar />
             <div>
                 <form>
                     <div>
-                        <div>
+                        <div className="space-x-2">
                             <label>Hero Name:</label>
-                            <Input />
+                            <Input type="text" placeholder="Hero Name" value={heroname} onchange={(evt) => setHeroname(evt.target.value)} />
                         </div>
                     </div>
+                    <button>update</button>
                 </form>
             </div>
         </div>
