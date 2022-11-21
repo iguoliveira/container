d750fa598b1e8dcd89eaef4d229042be435ef8d6
iguoliveira <igor.or042@gmail.com>
2022-11-21 11:29:14 -0300

    📦 NEW: Cars modal

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,14 +2,16 @@ import './card.scss'
 interface CardProps {
     number: string
     name: string
+    onClick?: any
 }
 
-export const Card = ({ number, name }: CardProps) => {
+export const Card = ({ number, name, onClick }: CardProps) => {
     return (
         <article className='card-area-content'>
             <span>Number: {number}</span>
             <span>|</span>
             <span>Name: {name}</span>
+            <button onClick={onClick}>View</button>
         </article>
     )
 }
--- src/pages/Areas.tsx
+++ src/pages/Areas.tsx
@@ -1,15 +1,34 @@
 import './areas.scss'
 import { Card } from '../components/Card'
 import data from '../data.json'
+import { useState } from 'react'
 
 export const Areas = () => {
+    document.title = 'Areas'
+    const [isModal, setIsModal] = useState(false)
+
+    function handleClick(number: number) {
+        if (number > 1) {
+            setIsModal(!isModal)
+        } else {
+            setIsModal(false)
+        }
+    }
+
     return (
         <section className='areas-content'>
+            {isModal && <div className='cars-container'>
+                {data.map((item, index) => {
+                    return item.cars.length > 1 && item.cars.map((item, index) => {
+                        return <div className='cars'>Car Model: {item.model}</div>
+                    })
+                })}
+            </div>}
             <article className='areas-name-container'>
                 <span className='header'>AREAS</span>
                 <div className='areas'>
                     {data.map((item, index) => {
-                        return <Card number={item.number} name={item.name} />
+                        return <Card number={item.name} name={item.name} onClick={() => handleClick(item.cars.length)} />
                     })}
                 </div>
             </article>
