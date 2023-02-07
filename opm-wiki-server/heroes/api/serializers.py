5d73726b9e0a3a39791c0fd6fc4a4ab1fe9920ee
iguoliveira <igor.or042@gmail.com>
2023-02-06 23:49:48 -0300

    cut: Wrong branch in api opm wiki

--- opm-wiki-server/heroes/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from heroes import models
-
-class HeroesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Heroes
-        fields = '__all__'
